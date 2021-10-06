5bbb72c289d5401858ee703f9212f2c3a06ca234
Preethi <preethiknot1998@gmail.com>
2021-10-06 19:43:33 +0530

    added logic

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -7,26 +7,66 @@ class Calculator extends React.Component {
         weight : ''
     }
     
-    onSubmit() {
-        console.log("submit");
-    }
-
     onInputAge = (event) => {
         let newAge = event.target.value;
         this.setState({age : newAge});
-        console.log(newAge);
+        // console.log(newAge);
     }
 
     onInputHeight = (event) => {
        let newHeight = event.target.value;
        this.setState({height : newHeight});
-        console.log(newHeight);
+        // console.log(newHeight);
     } 
     
     onInputWeight = (event) => {
         let newWeight = event.target.value;
         this.setState({weight : newWeight})
-        console.log(newWeight);
+        // console.log(newWeight);
+    }
+    
+    onSubmit = () => {
+        if(this.state.age.length && this.state.height.length && this.state.weight.length > 0 ) {
+            let bmi = this.calculateBMI();
+            try {
+            let status = this.calculateBMIStatus(bmi);
+            console.log(bmi,status);
+             }
+            catch (error) {
+            alert('Invalid Input');
+            return;
+            }
+        }
+        else {
+            alert('Please check the details entered');
+        }
+    }
+
+    calculateBMI() {
+        let bmi = (10000 * this.state.weight) / (this.state.height * this.state.height);
+        return bmi;
+    }
+
+
+    calculateBMIStatus = (bmi) => {
+        let status;
+        switch(true) {
+            case (bmi < 18.5) && (bmi >0):
+                status = "You are underweight";
+                break;
+                case (bmi < 25) && (bmi >= 18.5):
+                    status = "You are normal-weight";
+                    break;
+                case (bmi < 30) && (bmi >= 25):
+                    status = "You are over-weight";
+                    break;
+                case (bmi >= 30):
+                    status = "You are obese";
+                    break;
+                default:
+                    throw new Error("Invalid value");   
+        }
+        return status;
     }
 
     render() { 
